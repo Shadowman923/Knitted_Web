09c1dabdacb1a924913c2b880d1b95b6d2f587e0
Petr Velikanov <velikanov.petr@gmail.com>
2025-03-09 13:39:39 +0100

    feat: Add missing semicolon in storeZustand.tsx

--- store/storeZustand.tsx
+++ store/storeZustand.tsx
@@ -5,4 +5,4 @@ const useStore = create((set) => ({
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
   updateBears: (newBears) => set({ bears: newBears }),
-}))
+}));
